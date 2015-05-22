b37292328ecf9f367b1516e4d864ee4365196dd7
alpinus <dongcai.zhao@outlook.com>
2015-05-22 10:44:25 +0800

    记忆部分的选项按钮样式
    啊 好丑。老洺看看有没有办法弄好看点？

--- assets/js/remember.js
+++ assets/js/remember.js
@@ -1,8 +1,16 @@
 var total_id=10;
 var current_id=1;
+var show_ping=true;
+var show_pian=true;
+var show_luoma=true;
+var is_random=false;
 function init(){
 	//alert("init");
 	show_hiragana();
+	set_ping();
+	set_pian();
+	set_luoma();
+	set_random();
 }
 function next(){
 	if(current_id+1<total_id){
@@ -15,4 +23,56 @@ function next(){
 function show_hiragana() {
 	var img= $(".hiragana-img");
 	img.attr("src","assets/image/hiragana/"+current_id+".png");
+}
+function change_option(option_type){
+	switch(option_type){
+		case "ping":
+			show_ping=!show_ping;
+			set_ping();
+		break;
+		case "pian":
+			show_pian=!show_pian;
+			set_pian();
+		break;
+		case "luoma":
+			show_luoma=!show_luoma;
+			set_luoma();
+		break;
+		case "random":
+			is_random=!is_random;
+			set_random();
+		break;
+	}
+}
+function set_ping(){
+	var btn=$("#btn-ping");
+	if(show_ping){
+		btn.css("background-color","red");
+	}else{
+		btn.css("background-color","gray");
+	}
+}
+function  set_pian() {
+	var btn=$("#btn-pian");
+	if(show_pian){
+		btn.css("background-color","yellow");
+	}else{
+		btn.css("background-color","gray");
+	}
+}
+function set_luoma(){
+	var btn=$("#btn-luoma");
+	if(show_luoma){
+		btn.css("background-color","blue");
+	}else{
+		btn.css("background-color","gray");
+	}
+}
+function set_random() {
+	var btn=$("#btn-random");
+	if(is_random){
+		btn.css("background-color","green");
+	}else{
+		btn.css("background-color","gray");
+	}
 }
